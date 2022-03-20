188960494cdf56f49b4368849b1c896a4fc01292
hcl-smile <13215027210@163.com>
2022-03-20 12:49:52 +0800

    feat: fix baseurl

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 axios.create({
-  baseURL:
-    process.env['NODE_ENV'] === 'development'
-      ? undefined
-      : 'https://translate-weld.vercel.app',
+  baseURL: 'https://translate-weld.vercel.app',
 });
 
 export { axios };
